4c8a770c8efe0e460e79f84ad9967cf75345b02a
DamianoCelesti <damiano.celesti@gmail.com>
2025-02-07 08:28:24 +0100

    added post router index

---
+++ routers/posts.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const router = express.Router();
+// importo l'array
+// const posts = require('../data/posts');
+
+
+
+// importo le funzioni del controller
+const postController = require('../controllers/postController');
+
+
+
+// rotte di CRUD di posts
+// index
+router.get('/', postController.index);
+    
+    
+
+// show
+router.get('/:id', function (req, res) {
+    res.send('Dettagli del post numero ' + req.params.id);
+});
+// store
+router.post('/', function (req, res) {
+    res.send('Creazione nuovo post');
+});
+// update
+router.put('/:id', function (req, res) {
+    res.send('Modifica del post numero ' + req.params.id);
+});
+// modify
+router.patch('/:id', function (req, res) {
+    res.send('Modifica parziale del post numero ' + req.params.id);
+});
+// destroy
+router.delete('/:id', function (req, res) {
+    res.send('Eliminazione del post numero ' + req.params.id);
+});
+
+module.exports = router;
